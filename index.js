338e8aa3c0147d0e65d22083830f90f50bd3a7de
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-12-13 12:11:18 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function createTimeInEvent(employee, timeStamp){
     employee.timeInEvents.push({
         type: "TimeIn",
         hour: Number(hour),
-        date,
+        date: date
     })
     return employee
 }
